f784597a4b4f5ee9ca460af88f5a100dfce567e3
raywam.v.shinkoda <raywam@hotmail.com>
2020-10-15 11:08:45 -0300

    ajustes

--- teste/src/App.js
+++ teste/src/App.js
@@ -2,44 +2,33 @@ import React from 'react';
 
 var dataLayer = window.dataLayer || [];
 
-function reportClickButton1() {
+function reportSubmit() {
   dataLayer.push(
     {
       'event': 'submitForm',
       'eventCategory': 'clicks', 
-      'eventAction': 'click_button_1', 
+      'eventAction': 'click_button_submit', 
       'eventValue': 'admin',
     });
 }
 
-function reportMouseLeaveButton1() {
-  dataLayer.push(
-    {
-      'event': 'onMouseLeave',
-      'eventCategory': 'onMouseLeave', 
-      'eventAction': 'onMouseLeave_button_1', 
-      'eventValue': 'professor',
-    });
-}
 
 function App() {
-  function rendleClickButton1(event) {
+  function rendleSubmit(event) {
     event.preventDefault()
-    reportClickButton1()
+    reportSubmit()
   }
 
-  function rendleMouseLeaveButton1(event) {
-    event.preventDefault()
-    reportMouseLeaveButton1()
-  }
+
 
   return (
     <div className="App" style={{minHeight:1800 + 'px'}}>
       <p>v4</p>
-      <form id="signup-form" onSubmit={rendleClickButton1}>
-        <button type="submit" className="button2">Button 2</button>
+      <form id="signup-form" onSubmit={rendleSubmit}>
+        <input type="text"></input>
+        <button type="submit">Submit</button>
       </form>
-      <button onMouseLeave={rendleMouseLeaveButton1} className="button1">Button 1</button>
+      <br/>
       <a target="_blank" rel="noopener noreferrer" href="https://www.google.com">Google</a>
     </div>
   );
